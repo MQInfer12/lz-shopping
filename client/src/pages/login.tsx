599a6fad3d5ca0c830a61c3fce0d3c9a835d7604
JoshVComstock <josedanielveizaga@gmail.com>
2023-03-12 23:27:28 -0400

    Variable colores

---
+++ client/src/pages/login.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import styled from "styled-components";
+
+const Login = () => {
+  return (
+    <Container>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <div>
+          <h1>Mau Prins</h1>
+        </div>
+        <div>
+          <label htmlFor="">Usuario</label>
+          <input type="email" name="" id="" />
+        </div>
+        <div>
+          <label htmlFor="">Password</label>
+          <input type="password" name="" id="" />
+        </div>
+        <button>Ingresar</button>
+      </div>
+    </Container>
+  );
+};
+
+export default Login;
+
+const Container=styled.div`
+
+`;
---
+++ client/src/style/Varianbles.tsx
@@ -0,0 +1,12 @@
+export const colors={
+    primery:'#fff0f3',
+    Primary100:'#ffccd5',
+    Primary200:'#ffb3c1',
+    Primary300:'#ff8fa3',
+    Primary400:'#ff758f',
+    Primary500:'#ff4d6d',
+    Primary600:'#c9184a',
+    Primary700:'#a4133c',
+    Primary800:'#800f2f',
+    Primary900:'#590d22',
+}
