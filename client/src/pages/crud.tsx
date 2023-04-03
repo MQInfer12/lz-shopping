1df489962f9e3de081851ce0239df1e08ea83913
JoshVComstock <josedanielveizaga@gmail.com>
2023-04-03 09:57:46 -0400

    crud

--- client/src/pages/crud.tsx
+++ client/src/pages/crud.tsx
@@ -11,37 +11,47 @@ const Crud = () => {
   const handlesend = async () => {
     const res = await postCategory(name);
     addCategory(res.data);
-    setName('');
+    setName("");
   };
   const handledelete = async (id: number) => {
     const res = await deleteCategory(id);
     removeCategory(res.data);
   };
-  
   return (
-    <div>
+    <>
       <div>
-        <label htmlFor="">Categoria</label>
-        <input
-          type="text"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-        <button onClick={handlesend}>Agregar</button>
-      </div>
-      {categories.map((v, i) => (
-        <div key={v.id}>
-          <p>{v.name}</p>
-          <button
-            onClick={() => {
-              handledelete(v.id);
-            }}
-          >
-            X
-          </button>
+        <div>
+          <label htmlFor="">Categoria</label>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button onClick={handlesend}>Agregar</button>
         </div>
-      ))}
-    </div>
+        {categories.map((v, i) => (
+          <div key={v.id}>
+            <p>{v.name}</p>
+            <button
+              onClick={() => {
+                handledelete(v.id);
+              }}
+            >
+              X
+            </button>
+          </div>
+        ))}
+      </div>
+      <div>
+        <label htmlFor="">Producto</label>
+        <input type="text" placeholder="Name" />
+        <input type="number" placeholder="Price" />
+        <input type="number" placeholder="Stock" />
+        <input type="file" />
+        <img src="" alt="" />
+        <button>Agregar</button>
+      </div>
+    </>
   );
 };
 
